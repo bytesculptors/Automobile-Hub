067e62fb9c4063a72b5084ad98f59b18c5ecd50a
quanphung27-05 <100451205+quanphung27-05@users.noreply.github.com>
2024-04-11 21:29:37 +0700

    fix log out icon

--- front_end/src/Components/Home/Home_Components/Header/Header.jsx
+++ front_end/src/Components/Home/Home_Components/Header/Header.jsx
@@ -8,6 +8,8 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import { Badge } from "@mui/material";
 import LoginModal from "../UI/LoginModal";
 import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+
 import {
   Menu,
   MenuHandler,
@@ -169,7 +171,14 @@ const Header = () => {
                               navigate("/home");
                             }}
                           >
-                            <ArrowUturnLeftIcon className="h-4 w-4 mr-2" />
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-4 w-4 mr-2 font-bold"
+                               viewBox="0 0 512 512"
+                            >
+                              <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
+                            </svg>
+                           
                             Đăng xuất
                           </MenuItem>
                         </MenuList>
@@ -179,12 +188,14 @@ const Header = () => {
                 : (console.log(2),
                   (
                     <div className="header__top__right d-flex align-items-center justify-content-end gap-3 hover:bg-gray-200 p-1 rounded-lg">
-                      <Link
-                        to="/login"
-                        className="d-flex align-items-center gap-1 "
-                      >
-                        <i className="ri-user-shared-line text-gray-500 text-2xl"></i>
-                      </Link>
+                      <Tooltip title="Đăng nhập tài khoản">
+                        <Link
+                          to="/login"
+                          className="d-flex align-items-center gap-1 "
+                        >
+                          <i className="ri-user-shared-line text-gray-500 text-2xl"></i>
+                        </Link>
+                      </Tooltip>
                     </div>
                   ))}
             </Col>
